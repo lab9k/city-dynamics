0b8fc220679f6e8b025639beca7d68d4923cd4b2
Jerome <jerome@xomnia.com>
2017-11-23 16:31:20 +0100

    reverted run_additional_sql.py

--- importer/run_additional_sql.py
+++ importer/run_additional_sql.py
@@ -31,74 +31,6 @@ def create_geometry_query(tablename):
   CREATE INDEX {1} ON "{0}" USING GIST(geom);
   """.format(tablename, 'geom_' + tablename)
 
-
-def add_buurtcombinatie_query(table):
-  return """
-  SELECT "{0}".id, BUURTCOMBINATIE.Stadsdeel_code
-  FROM "{0}", BUURTCOMBINATIE
-  WHERE ST_Intersects("{0}".geom, BUURTCOMBINATIE.COORDS);
-  """.format(table)
-
-def test(table):
-  return """
-  SELECT 
-  g.id as id,
-  b."Buurtcombinatie" as bc
-  FROM 
-  gvb as g, 
-  buurtcombinatie as b
-  WHERE ST_Intersects(g.geom, b."COORDS")
-  """
-
-
-def dummy(table):
-  return"""SELECT 
-  e.*, 
-  g.naam as gebiedsnaam, 
-  g.code as gebiedscode
-  INTO 
-    crowscores_totaal
-  FROM
-     (SELECT 
-       c.*, 
-       d.stadsdeelcode,
-       d.buurtcode,
-       d.wijkcode,
-       d.stadsdeelnaam,
-       d.buurtnaam,
-       d.wijknaam
-    FROM 
-      (select * from crowscores) as c, 
-      (SELECT 
-         a.stadsdeelcode,
-         a.buurtcode,
-         w.vollcode as wijkcode,
-         a.stadsdeelnaam,
-         a.buurtnaam,
-         w.naam as wijknaam,
-         a.wkb_geometry
-       FROM
-       (  SELECT 
-          s.naam as stadsdeelnaam,
-          s.code as stadsdeelcode,
-          b.naam as buurtnaam,
-          s.code || b.code as buurtcode,
-          b.wkb_geometry
-          FROM 
-            buurt as b, 
-            stadsdeel as s
-          WHERE ST_WITHIN(ST_CENTROID(b.wkb_geometry),s.wkb_geometry)
-        ) as a, 
-        buurtcombinatie as w
-      WHERE ST_WITHIN(ST_CENTROID(a.wkb_geometry),w.wkb_geometry)) as d
-    WHERE ST_WITHIN(ST_CENTROID(c.geom), d.wkb_geometry)
-    ) as e,
-    gebiedsgerichtwerken as g
-  WHERE ST_WITHIN(ST_CENTROID(e.geom), g.wkb_geometry);
-  ALTER TABLE crowscores_totaal 
-  ADD PRIMARY KEY (id);
-  """
-
 def execute_sql(pg_str, sql):
     with psycopg2.connect(pg_str) as conn:
         with conn.cursor() as cursor:
@@ -115,10 +47,7 @@ def main(dbConfig):
     pg_str = get_pg_str(config_auth.get(dbConfig,'host'),config_auth.get(dbConfig,'port'),config_auth.get(dbConfig,'dbname'), config_auth.get(dbConfig,'user'), config_auth.get(dbConfig,'password'))
     for table in tables_to_modify:
         print(table)
-        #execute_sql(pg_str, create_geometry_query(table))
-        #print(add_buurtcombinatie_query(table))
-        #execute_sql(pg_str, add_buurtcombinatie_query(table))
-        execute_sql(pg_str, test(table))
+        execute_sql(pg_str, create_geometry_query(table))
 
 
 if __name__ == '__main__':
