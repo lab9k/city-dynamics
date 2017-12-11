677077c581f4689a70bbb2d39be9865662b9595e
Jerome <jerome@xomnia.com>
2017-12-11 11:24:50 +0100

    deleted obsolete lines

--- web/citydynamics/api/views.py
+++ web/citydynamics/api/views.py
@@ -12,6 +12,7 @@ from . import serializers
 import datetime
 import django_filters
 
+
 def convert_to_date(input_date):
     try:
         date_obj = datetime.datetime.strptime(input_date, '%Y-%m-%d-%H-%M-%S')
@@ -19,9 +20,8 @@ def convert_to_date(input_date):
         date_obj = None
     return date_obj
 
-class DateFilter(FilterSet):
-    #datetime = django_filters.DateTimeFilter(action=filter_on_date)
 
+class DateFilter(FilterSet):
     vanaf = filters.CharFilter(label='vanaf', method='vanaf_filter')
     tot = filters.CharFilter(label='tot', method='tot_filter')
     op = filters.CharFilter(label='op', method='op_filter')
@@ -39,7 +39,8 @@ class DateFilter(FilterSet):
     def vanaf_filter(self, queryset, _name, value):
         date = convert_to_date(value)
         if not date:
-            raise ValidationError('Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
+            raise ValidationError(
+                'Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
         queryset = queryset.filter(timestamp__gte=date)
 
         return queryset
@@ -47,7 +48,8 @@ class DateFilter(FilterSet):
     def tot_filter(self, queryset, _name, value):
         date = convert_to_date(value)
         if not date:
-            raise ValidationError('Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
+            raise ValidationError(
+                'Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
         queryset = queryset.filter(timestamp__lte=date)
 
         return queryset
@@ -55,7 +57,8 @@ class DateFilter(FilterSet):
     def op_filter(self, queryset, _name, value):
         date = convert_to_date(value)
         if not date:
-            raise ValidationError('Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
+            raise ValidationError(
+                'Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
         queryset = queryset.filter(timestamp=date)
 
         return queryset
@@ -70,46 +73,3 @@ class DrukteindexViewSet(rest.DatapuntViewSet):
     serializer_detail_class = serializers.DrukteindexSerializer
     queryset = models.Drukteindex.objects.all()
     filter_class = DateFilter
-
-    # def get_queryset(self):
-    #     queryset = models.Drukteindex.objects.all()
-    #     datetime = self.request.query_params.get('datetime', None)
-    #     if datetime is not None:
-    #         queryset = models.Drukteindex.objects.filter(timestamp=datetime)
-    #     return queryset
-
-
-    # def get_queryset(self, *args, **kwargs):
-    #     _, queryset =  self._get_filter_and_queryset_by_date(self.queryset, self.request)
-    #     return queryset
-
-    # def _get_filter_and_queryset_by_date(self, queryset, request):
-    #     adjacent_filter = {}
-
-    #     start_date = convert_to_date(request, 'vanaf')
-    #     if start_date is not None:
-    #         adjacent_filter['vanaf'] = start_date
-    #         queryset = queryset.filter(timestamp__gte=start_date)
-
-    #     end_date = convert_to_date(request, 'tot')
-    #     if end_date is not None:
-    #         adjacent_filter['tot'] = end_date
-    #         queryset = queryset.filter(timestamp__lt=end_date)
-
-    #     on_date = convert_to_date(request, 'op')
-    #     if end_date is not None:
-    #         adjacent_filter['op'] = on_date
-    #         queryset = queryset.filter(timestamp=on_date)
-
-    #     return adjacent_filter, queryset
-
-
-# def filter_on_date(queryset, value):
-#     #assert False
-#     if not value:
-#         return queryset
-#     date = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
-#     return queryset.filter(timestamp = date)
-
-
-
