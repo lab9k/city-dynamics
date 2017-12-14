555baa863af53e0533abe620f5e3c21e6c281087
Jerome <jerome@xomnia.com>
2017-12-14 14:31:55 +0100

    recent measurements API

--- web/citydynamics/api/models.py
+++ web/citydynamics/api/models.py
@@ -5,79 +5,28 @@ from datetime import datetime
 from django.contrib.gis.db import models as geo
 
 
-class Cmsa(models.Model):
-    index = models.BigIntegerField(primary_key=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
-    # Field name made lowercase. Field renamed to remove unsuitable
-    # characters. Field renamed because it started with '_'.
-    field_in = models.BigIntegerField(db_column=' In', blank=True, null=True)
-    # Field name made lowercase. Field renamed to remove unsuitable
-    # characters. Field renamed because it started with '_'.
-    field_out = models.BigIntegerField(db_column=' Out', blank=True, null=True)
-    cam_number = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'CMSA'
-
-
 class Functiekaart(models.Model):
     index = models.BigIntegerField(primary_key=True)
-    # Field name made lowercase.
-    objectnummer = models.BigIntegerField(
-        db_column='OBJECTNUMMER', blank=True, null=True)
-    # Field name made lowercase.
-    functie2_oms = models.TextField(
-        db_column='FUNCTIE2_OMS', blank=True, null=True)
-    # Field name made lowercase.
-    zaaknaam = models.TextField(db_column='ZAAKNAAM', blank=True, null=True)
-    # Field name made lowercase.
-    functie2_eng = models.TextField(
-        db_column='FUNCTIE2_ENG', blank=True, null=True)
-    # Field name made lowercase.
-    functie1_id = models.TextField(
-        db_column='FUNCTIE1_ID', blank=True, null=True)
-    # Field name made lowercase.
-    functie1_oms = models.TextField(
-        db_column='FUNCTIE1_OMS', blank=True, null=True)
-    # Field name made lowercase.
-    functie1_eng = models.TextField(
-        db_column='FUNCTIE1_ENG', blank=True, null=True)
-    # Field name made lowercase.
-    functie2_id = models.TextField(
-        db_column='FUNCTIE2_ID', blank=True, null=True)
-    # Field name made lowercase.
-    laag = models.BigIntegerField(db_column='LAAG', blank=True, null=True)
-    # Field name made lowercase.
-    adressen_lijst = models.TextField(
-        db_column='ADRESSEN_LIJST', blank=True, null=True)
-    # Field name made lowercase.
-    adressen_vot = models.TextField(
-        db_column='ADRESSEN_VOT', blank=True, null=True)
-    # Field name made lowercase.
-    adressen_aantal = models.BigIntegerField(
-        db_column='ADRESSEN_AANTAL', blank=True, null=True)
-    # Field name made lowercase.
-    oppervlakte_som = models.BigIntegerField(
-        db_column='OPPERVLAKTE_SOM', blank=True, null=True)
-    # Field name made lowercase.
-    oppervlakte_nul = models.BigIntegerField(
-        db_column='OPPERVLAKTE_NUL', blank=True, null=True)
-    # Field name made lowercase.
-    checkdatum = models.TextField(
-        db_column='CHECKDATUM', blank=True, null=True)
-    # Field name made lowercase.
-    zaak_id = models.BigIntegerField(
-        db_column='ZAAK_ID', blank=True, null=True)
-    # Field name made lowercase.
-    coords = models.TextField(db_column='COORDS', blank=True, null=True)
-    # Field name made lowercase.
-    lng = models.TextField(db_column='LNG', blank=True, null=True)
-    # Field name made lowercase.
-    lat = models.TextField(db_column='LAT', blank=True, null=True)
-    # Field name made lowercase. Field renamed to remove unsuitable characters.
-    unnamed_19 = models.FloatField(
-        db_column='Unnamed: 19', blank=True, null=True)
+    objectnummer = models.BigIntegerField(db_column='OBJECTNUMMER', blank=True, null=True)  # Field name made lowercase.
+    functie2_oms = models.TextField(db_column='FUNCTIE2_OMS', blank=True, null=True)  # Field name made lowercase.
+    zaaknaam = models.TextField(db_column='ZAAKNAAM', blank=True, null=True)  # Field name made lowercase.
+    functie2_eng = models.TextField(db_column='FUNCTIE2_ENG', blank=True, null=True)  # Field name made lowercase.
+    functie1_id = models.TextField(db_column='FUNCTIE1_ID', blank=True, null=True)  # Field name made lowercase.
+    functie1_oms = models.TextField(db_column='FUNCTIE1_OMS', blank=True, null=True)  # Field name made lowercase.
+    functie1_eng = models.TextField(db_column='FUNCTIE1_ENG', blank=True, null=True)  # Field name made lowercase.
+    functie2_id = models.TextField(db_column='FUNCTIE2_ID', blank=True, null=True)  # Field name made lowercase.
+    laag = models.BigIntegerField(db_column='LAAG', blank=True, null=True)  # Field name made lowercase.
+    adressen_lijst = models.TextField(db_column='ADRESSEN_LIJST', blank=True, null=True)  # Field name made lowercase.
+    adressen_vot = models.TextField(db_column='ADRESSEN_VOT', blank=True, null=True)  # Field name made lowercase.
+    adressen_aantal = models.BigIntegerField(db_column='ADRESSEN_AANTAL', blank=True, null=True)  # Field name made lowercase.
+    oppervlakte_som = models.BigIntegerField(db_column='OPPERVLAKTE_SOM', blank=True, null=True)  # Field name made lowercase.
+    oppervlakte_nul = models.BigIntegerField(db_column='OPPERVLAKTE_NUL', blank=True, null=True)  # Field name made lowercase.
+    checkdatum = models.TextField(db_column='CHECKDATUM', blank=True, null=True)  # Field name made lowercase.
+    zaak_id = models.BigIntegerField(db_column='ZAAK_ID', blank=True, null=True)  # Field name made lowercase.
+    coords = models.TextField(db_column='COORDS', blank=True, null=True)  # Field name made lowercase.
+    lng = models.TextField(db_column='LNG', blank=True, null=True)  # Field name made lowercase.
+    lat = models.TextField(db_column='LAT', blank=True, null=True)  # Field name made lowercase.
+    unnamed_19 = models.FloatField(db_column='Unnamed: 19', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
 
     class Meta:
         managed = False
@@ -88,7 +37,7 @@ class Verblijversindex(models.Model):
     index = models.BigIntegerField(primary_key=True)
     wijk = models.TextField(blank=True, null=True)
     oppervlakte_m2 = models.FloatField(blank=True, null=True)
-    verblijversindex = models.BigIntegerField(blank=True, null=True)
+    verblijversindex = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -121,22 +70,11 @@ class Buurtcombinatie(models.Model):
     type = models.CharField(max_length=255, blank=True, null=True)
     uri = models.CharField(max_length=255, blank=True, null=True)
     wkb_geometry = models.GeometryField(blank=True, null=True)
-    wkb_geometry_simplified = models.GeometryField(
-        srid=0, blank=True, null=True)
+    wkb_geometry_simplified = models.GeometryField(srid=0, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'buurtcombinatie'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
 
 
 class DjangoMigrations(models.Model):
@@ -151,40 +89,17 @@ class DjangoMigrations(models.Model):
 
 class Drukteindex(models.Model):
     index = models.BigIntegerField(primary_key=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
     vollcode = models.TextField(blank=True, null=True)
-    ogc_fid = models.BigIntegerField(blank=True, null=True)
-    gml_id = models.TextField(blank=True, null=True)
-    id = models.TextField(blank=True, null=True)
-    naam = models.TextField(blank=True, null=True)
-    display = models.TextField(blank=True, null=True)
-    type = models.TextField(blank=True, null=True)
-    uri = models.TextField(blank=True, null=True)
-    wkb_geometry = geo.PolygonField(null=True, srid=28992)
-    wkb_geometry_simplified = models.TextField(blank=True, null=True)
-    oppervlakte_m2 = models.FloatField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    google_live_normalized = models.FloatField(blank=True, null=True)
+    google_week_normalized = models.FloatField(blank=True, null=True)
+    gvb_buurt_normalized = models.FloatField(blank=True, null=True)
+    verblijversindex_normalized = models.FloatField(blank=True, null=True)
     drukte_index = models.FloatField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'drukteindex'
-
-
-class DrukteindexTest(models.Model):
-    index = models.BigIntegerField(primary_key=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
-    vollcode = models.TextField(blank=True, null=True)
-    ogc_fid = models.BigIntegerField(blank=True, null=True)
-    gml_id = models.TextField(blank=True, null=True)
-    id = models.TextField(blank=True, null=True)
-    naam = models.TextField(blank=True, null=True)
-    display = models.TextField(blank=True, null=True)
-    type = models.TextField(blank=True, null=True)
-    uri = models.TextField(blank=True, null=True)
-    wkb_geometry = geo.PolygonField(null=True, srid=28992)
-    wkb_geometry_simplified = models.TextField(blank=True, null=True)
-    oppervlakte_m2 = models.FloatField(blank=True, null=True)
-    drukte_index = models.FloatField(blank=True, null=True)
 
 
 class Gebiedsgerichtwerken(models.Model):
@@ -205,8 +120,7 @@ class Gebiedsgerichtwerken(models.Model):
 
 class Google(models.Model):
     index = models.BigIntegerField(primary_key=True)
-    # Field name made lowercase.
-    location = models.TextField(db_column='Location', blank=True, null=True)
+    location = models.TextField(db_column='Location', blank=True, null=True)  # Field name made lowercase.
     timestamp = models.DateTimeField(blank=True, null=True)
     historical = models.FloatField(blank=True, null=True)
     live = models.FloatField(blank=True, null=True)
@@ -224,10 +138,46 @@ class Google(models.Model):
         db_table = 'google'
 
 
+class GoogleDec(models.Model):
+    index = models.BigIntegerField(primary_key=True)
+    place_id = models.TextField(blank=True, null=True)
+    historical = models.FloatField(blank=True, null=True)
+    live = models.FloatField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    types = models.TextField(blank=True, null=True)
+    geom = models.GeometryField(srid=0, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'google_dec'
+
+
+class GoogleDecWithBc(models.Model):
+    index = models.BigIntegerField(primary_key=True)
+    place_id = models.TextField(blank=True, null=True)
+    historical = models.FloatField(blank=True, null=True)
+    live = models.FloatField(blank=True, null=True)
+    timestamp = models.DateTimeField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    types = models.TextField(blank=True, null=True)
+    geom = models.GeometryField(srid=0, blank=True, null=True)
+    vollcode = models.CharField(max_length=255, blank=True, null=True)
+    naam = models.CharField(max_length=255, blank=True, null=True)
+    stadsdeel_code = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'google_dec_with_bc'
+
+
 class GoogleWithBc(models.Model):
     index = models.BigIntegerField(primary_key=True)
-    # Field name made lowercase.
-    location = models.TextField(db_column='Location', blank=True, null=True)
+    location = models.TextField(db_column='Location', blank=True, null=True)  # Field name made lowercase.
     timestamp = models.DateTimeField(blank=True, null=True)
     historical = models.FloatField(blank=True, null=True)
     live = models.FloatField(blank=True, null=True)
@@ -281,37 +231,6 @@ class GvbWithBc(models.Model):
     class Meta:
         managed = False
         db_table = 'gvb_with_bc'
-
-
-class Mora(models.Model):
-    index = models.BigIntegerField(primary_key=True)
-    hoofdrubriek = models.TextField(blank=True, null=True)
-    subrubriek = models.TextField(blank=True, null=True)
-    lat = models.FloatField(blank=True, null=True)
-    lon = models.FloatField(blank=True, null=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
-    geom = models.GeometryField(srid=0, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'mora'
-
-
-class MoraWithBc(models.Model):
-    index = models.BigIntegerField(primary_key=True)
-    hoofdrubriek = models.TextField(blank=True, null=True)
-    subrubriek = models.TextField(blank=True, null=True)
-    lat = models.FloatField(blank=True, null=True)
-    lon = models.FloatField(blank=True, null=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
-    geom = models.GeometryField(srid=0, blank=True, null=True)
-    vollcode = models.CharField(max_length=255, blank=True, null=True)
-    naam = models.CharField(max_length=255, blank=True, null=True)
-    stadsdeel_code = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'mora_with_bc'
 
 
 class Stadsdeel(models.Model):
--- web/citydynamics/api/views.py
+++ web/citydynamics/api/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import FilterSet
 from django_filters.rest_framework import filters
 from rest_framework.serializers import ValidationError
+from django_filters.rest_framework import DjangoFilterBackend
 
 from datapunt_api import rest
 from . import models
@@ -15,6 +16,7 @@ log = logging.getLogger(__name__)
 def convert_to_date(input_date):
     try:
         date_obj = datetime.datetime.strptime(input_date, '%Y-%m-%d-%H-%M-%S')
+        #date_obj = datetime.datetime.strptime(input_date, '%Y-%m-%d-%H')
     except ValueError:
         log.exception("Got an invalid date value")
         date_obj = None
@@ -63,13 +65,122 @@ class DateFilter(FilterSet):
 
         return queryset
 
+    # def datetime_filter(self, queryset, _name, value, **kwargs):
+    #     date = convert_to_date(value)
+    #     if not date:
+    #         raise ValidationError(
+    #             'Please insert a datetime Year-Month-Day-Hour-Minute-Second, like: 2017-10-26-16-00-00')
+    #     log.debug(kwargs)
+    #     queryset = queryset.filter(kwargs['timestamp'])
+
+    #     return queryset
+
+    # def op_filter(self, queryset, _name, value):
+    #     queryset = datetime_filter(**{'timestamp':'date'})
+    #     return queryset
+
 
 class DrukteindexViewSet(rest.DatapuntViewSet):
     """
     Drukteindex API
     """
 
-    serializer_class = serializers.DrukteindexSerializer
-    serializer_detail_class = serializers.DrukteindexSerializer
-    queryset = models.Drukteindex.objects.all()
+    serializer_class = serializers.DrukteIndexSerializer
+    serializer_detail_class = serializers.DrukteIndexSerializer
+    queryset = models.Drukteindex.objects.order_by("index")
     filter_class = DateFilter
+
+
+# class RecentIndexFilter(FilterSet):
+
+#     latest_measures = filters.CharFilter(
+#         label='latest_measures', method='latest_filter')
+#     vollcode = filters.CharFilter(
+#         label='vollcode_filter', method='vollcode_filter')
+
+#     class Meta:
+#         model = models.Drukteindex
+#         fields = [
+#             'latest_measures',
+#             'vollcode'
+#         ]
+
+#     def latest_filter(self, queryset, _name, value):
+#         datetime = self.request.query_params.get('datetime', None)
+#         start_datetime = convert_to_date(datetime)
+#         end_datetime = start_datetime - datetime.timedelta(hours=24)
+#         queryset = queryset.filter(
+#             timestamp__range=(start_datetime, end_datetime))
+#         return queryset
+
+#     def vollcode_filter(self, queryset, _name, value):
+#         vollcode = self.request.query_params.get('vollcode', None)
+#         if vollcode is not None:
+#             queryset = queryset.filter(vollcode=vollcode)
+#         return queryset
+
+
+class RecentIndexViewSet(rest.DatapuntViewSet):
+    """
+    Recent index API
+    """
+
+    serializer_class = serializers.RecentIndexSerializer
+    serializer_detail_class = serializers.RecentIndexSerializer
+    queryset = models.Drukteindex.objects.order_by("index")
+    #filter_class = RecentIndexFilter
+
+    # def latest_filter(self, queryset, _name, value):
+    #     datetime = self.request.query_params.get('datetime', None)
+    #     start_datetime = convert_to_date(datetime)
+    #     end_datetime = start_datetime - datetime.timedelta(hours=24)
+    #     queryset = queryset.filter(
+    #         timestamp__range=(start_datetime, end_datetime))
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = models.Drukteindex.objects.all()
+
+        vollcode = self.request.query_params.get('vollcode', None)
+        if vollcode is not None:
+            queryset = queryset.filter(vollcode=vollcode)
+
+
+        timestamp_str = self.request.query_params.get('timestamp', None)
+
+        if timestamp_str is None:
+            timestamp_dt = datetime.datetime.now()
+
+        if timestamp_str is not None:
+            timestamp_dt = convert_to_date(timestamp_str)
+
+        if timestamp_dt > convert_to_date('2017-12-07-00-00-00'):
+            current_day = datetime.datetime.now().strftime("%A")
+            if current_day == 'Friday':
+                end_timestamp = '2017-12-02-23-00-00'                
+            elif current_day == 'Saturday':
+                end_timestamp = '2017-12-01-23-00-00'
+            elif current_day == 'Sunday':
+                end_timestamp = '2017-12-03-23-00-00'
+            elif current_day == 'Monday':
+                end_timestamp = '2017-12-04-23-00-00'
+            elif current_day == 'Tuesday':
+                end_timestamp = '2017-12-05-23-00-00'
+            elif current_day == 'Wednesday':
+                end_timestamp = '2017-12-06-23-00-00'
+            elif current_day == 'Thursday':
+                end_timestamp = '2017-12-07-23-00-00'
+            end_timestamp = convert_to_date(end_timestamp)
+            start_timestamp = end_timestamp - datetime.timedelta(hours=23)
+
+        else:
+            end_timestamp = timestamp_dt
+            start_timestamp = end_timestamp - datetime.timedelta(hours=22)
+
+        queryset = queryset.filter(
+            timestamp__range=(start_timestamp, end_timestamp))
+
+        return queryset
