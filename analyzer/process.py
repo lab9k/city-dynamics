fde369bea1d441dc24b5ced7822869bcc3fc735a
Thomas Jongstra <jongstra@gmail.com>
2018-01-29 18:44:09 +0100

    This commit adds a placeholder "process.py" file with some initial documentation.

---
+++ analyzer/process.py
@@ -0,0 +1,23 @@
+"""
+Module should implement class 'Process'.
+
+For each dataset, a subclass of class 'Process' is created to load and pre-process this dataset.
+
+
+# Implemented methods of class 'Process':
+
+* def database_connection()
+* def import()
+* def normalize()
+* def min-max()
+* def rename()  # Optional: to rename columns
+
+# Children of class 'Process':
+
+* Class Process_gvb(Process)
+* Class Process_google(Process)
+* Class Process_verblijversindex(Process)
+* Class Process_tellus(Process)
+* Etc..
+"""
+
