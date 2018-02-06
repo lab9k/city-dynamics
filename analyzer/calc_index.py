c09dbea47fca19850e091a3f03afc1bb8480cc15
jeromecremers <jerome@xomnia.com>
2018-02-06 11:16:43 +0100

    removed matplotlib from imports

--- analyzer/calc_index.py
+++ analyzer/calc_index.py
@@ -12,8 +12,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from datetime import timedelta
 
-import matplotlib
-
 from sklearn.preprocessing import MinMaxScaler
 
 config_auth = configparser.RawConfigParser()
