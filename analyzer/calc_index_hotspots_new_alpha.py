92a88645e3601b70d0601d372c197d2161218d9c
jeromecremers <jerome@xomnia.com>
2018-03-04 21:19:00 +0100

    removed 3 hotspots from file on objectstore

---
+++ analyzer/calc_index_hotspots_new_alpha.py
@@ -0,0 +1,199 @@
+import configparser
+import argparse
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+import pandas as pd
+import numpy as np
+import itertools
+
+config_auth = configparser.RawConfigParser()
+config_auth.read('auth.conf')
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+def get_conn(dbconfig):
+    """Create a connection to the database."""
+    postgres_url = URL(
+        drivername='postgresql',
+        username=config_auth.get(dbconfig, 'user'),
+        password=config_auth.get(dbconfig, 'password'),
+        host=config_auth.get(dbconfig, 'host'),
+        port=config_auth.get(dbconfig, 'port'),
+        database=config_auth.get(dbconfig, 'dbname')
+    )
+    conn = create_engine(postgres_url)
+    return conn
+
+
+def set_primary_key(table):
+    return """
+  ALTER TABLE "{}" ADD PRIMARY KEY (index)
+  """.format(table)
+
+# COPIED from main.py
+def linear_model(drukte):
+    # Normalise verblijversindex en gvb
+    # drukte['verblijvers_ha_2016'] = process.norm(drukte.verblijvers_ha_2016)
+    # drukte['gvb'] = process.norm(drukte.gvb)
+
+    drukte['drukte_index'] = 0
+
+    # make sure the sum of the weights != 0
+    linear_weigths = {'verblijvers_ha_2016': 1,
+                      'gvb': 8,
+                      'alpha_week': 2}
+
+    lw_normalize = sum(linear_weigths.values())
+
+    for col, weight in linear_weigths.items():
+        if col in drukte.columns:
+            drukte['drukte_index'] = drukte['drukte_index'].add(drukte[col] * weight, fill_value=0)
+
+    drukte['drukte_index'] = drukte['drukte_index'] / lw_normalize
+
+    # Sort values
+    drukte = drukte.sort_values(['vollcode', 'weekday', 'hour'])
+
+    return drukte
+
+
+def main():
+    conn = get_conn(dbconfig=args.dbConfig[0])
+
+    sql_query = """ SELECT * FROM "{}" """
+
+    hotspots_df = pd.read_sql("""SELECT * FROM hotspots""", conn)
+
+    drop_point_sm_hotspots = """
+    ALTER TABLE "hotspots"
+    DROP COLUMN IF EXISTS point_sm;
+    """
+
+    log.debug('Creating geometries on hotspots..')
+    create_geom_hotspots = """
+    ALTER TABLE hotspots
+    ADD COLUMN point_sm geometry;
+    UPDATE hotspots SET point_sm = ST_TRANSFORM( ST_SETSRID ( ST_POINT( "lon", "lat"), 4326), 3857)
+    """
+
+    conn.execute(drop_point_sm_hotspots)
+    conn.execute(create_geom_hotspots)
+    log.debug('..done.')
+
+    drop_point_sm_google = """
+    ALTER TABLE "alpha_locations_expected"
+    DROP COLUMN IF EXISTS point_sm;
+    """
+
+    log.debug('Creating geometries on Google locations..')
+    create_geom_google = """
+    ALTER TABLE alpha_locations_expected
+    ADD COLUMN point_sm geometry;
+    UPDATE alpha_locations_expected SET point_sm = ST_TRANSFORM( ST_SETSRID ( ST_POINT( "lon", "lat"), 4326), 3857)
+    """
+
+    conn.execute(drop_point_sm_google)
+    conn.execute(create_geom_google)
+    log.debug('..done.')
+
+    log.debug('Linking Google locations to hotspots..')
+    join_hotspots_query = """
+      DROP TABLE IF EXISTS alpha_locations_expected_hotspots;
+      create
+      table
+        alpha_locations_expected_hotspots as with hs as(
+          select
+            *
+          from
+            "hotspots"
+        ) select
+          hs."Hotspot",
+          alpha_locations_expected.*
+        from
+          alpha_locations_expected join hs on
+          st_intersects(
+            alpha_locations_expected.point_sm,
+            ST_BUFFER(hs.point_sm, 100)
+          )
+      """
+
+    conn.execute(join_hotspots_query)
+    log.debug('..done.')
+
+    google_hotspots = pd.read_sql(sql="SELECT * FROM alpha_locations_expected_hotspots", con=conn)
+
+    # historical weekpatroon
+    # first calculate the average weekpatroon per location
+    google_week_location = google_hotspots.groupby([
+        'hour', 'Hotspot', 'name'])['expected'].mean().reset_index()
+
+    # and then calculate the average weekpatroon per hotspot
+    google_week_hotspots = google_week_location.groupby([
+        'Hotspot', 'hour'])['expected'].mean().reset_index()
+
+    google_week_hotspots.rename(columns={'expected': 'alpha_week'}, inplace=True)
+
+    # fill the dataframe with all missing hotspot-hour combinations
+    x = {"weekday": np.arange(7), "hour": np.arange(24), "Hotspot": hotspots_df['Hotspot'].unique().tolist()}
+    hs_hour_combinations = pd.DataFrame(list(itertools.product(*x.values())), columns=x.keys())
+    google_week_hotspots = google_week_hotspots.merge(hs_hour_combinations, on=['hour', 'Hotspot'], how='outer')
+    google_week_hotspots['alpha_week'].fillna(value=0, inplace=True)
+
+    google_week_hotspots = google_week_hotspots.merge(google_hotspots[['Hotspot', 'vollcode']]
+                                                      .drop_duplicates('Hotspot'), on='Hotspot')
+
+    di = pd.read_sql(sql="SELECT * FROM drukteindex_hour_week", con=conn)
+
+    drukteindex_hotspots = google_week_hotspots.merge(di[['index',
+                                                          'vollcode',
+                                                          'weekday',
+                                                          'hour',
+                                                          'verblijvers_ha_2016',
+                                                          'gvb']], on=['vollcode', 'weekday', 'hour'], how='left')
+
+    drukteindex_hotspots = linear_model(drukteindex_hotspots)
+
+    drukteindex_hotspots = drukteindex_hotspots[['Hotspot', 'hour', 'weekday', 'drukte_index']]
+
+    drukteindex_hotspots.rename(columns={'drukte_index': 'drukteindex'}, inplace=True)
+
+    log.debug('Writing to db..')
+    drukteindex_hotspots.to_sql(name='drukteindex_hotspots_new_alpha', con=conn, if_exists='replace')
+
+    insert_into_models_hotspots = """
+    insert into datasets_hotspotsdrukteindex (
+    index,
+    hour,
+    weekday,
+    drukteindex,
+    hotspot_id
+    ) select c.index, hour, weekday, drukteindex, h.index from hotspots h, drukteindex_hotspots c
+    where  h."Hotspot" = c."Hotspot";
+
+    insert into datasets_hotspots (
+    index, 
+    hotspot, 
+    lat, 
+    lon
+    )
+    select index, "Hotspot", "lat", "lon" from hotspots;
+
+    """
+
+    conn.execute(insert_into_models_hotspots)
+
+    log.debug('done.')
+
+
+if __name__ == '__main__':
+    desc = "Calculate index hotspots."
+    log.debug(desc)
+    parser = argparse.ArgumentParser(desc)
+    parser.add_argument(
+        'dbConfig', type=str,
+        help='database config settings: dev or docker',
+        nargs=1)
+    args = parser.parse_args()
+    main()
