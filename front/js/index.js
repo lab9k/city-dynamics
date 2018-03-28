6e4b4ed2aff514027b9512ff569a1cd2c5373478
Stephan Preeker <stephan@preeker.net>
2018-03-28 17:06:29 +0200

    fix production url

--- front/js/index.js
+++ front/js/index.js
@@ -37,12 +37,13 @@ var geomap3 = 'https://t1.data.amsterdam.nl/topo_wm_light/{z}/{x}/{y}.png';
 // Initially assume we have the API running locally.
 var origin = 'http://127.0.0.1:8117';
 // var origin = 'https://acc.citydynamics.amsterdam.nl/api';
+// var origin = 'https://citydynamics.amsterdam.nl/api';
 
 // When using the production server, get the API from there.
 // TODO: Update this when the website name becomes "drukteradar.nl" or something alike.
-if(window.location.href.indexOf('prod.citydynamics.amsterdam') > -1)
+if(window.location.href.indexOf('citydynamics.amsterdam.nl') > -1)
 {
-	var origin = 'https://prod.citydynamics.amsterdam.nl/api';
+	var origin = 'https://citydynamics.amsterdam.nl/api';
 }
 
 // However, when using the acceptation server, get the API from there.
