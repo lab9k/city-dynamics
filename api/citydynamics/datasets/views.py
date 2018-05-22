668ea325c9591d756482313bce2ff11f30b72c28
Stephan Preeker <stephan@preeker.net>
2018-05-22 17:48:21 +0200

    historian

--- api/citydynamics/datasets/views.py
+++ api/citydynamics/datasets/views.py
@@ -206,7 +206,7 @@ def get_latest(api_source):
         latest = (
             models.RealtimeHistorian.objects
             .order_by('scraped_at')
-            .get(source=api_source).first()
+            .filter(source=api_source).first()
         )
     except models.RealtimeHistorian.DoesNotExist:
         return None
@@ -214,7 +214,7 @@ def get_latest(api_source):
     if not latest:
         return None
 
-    if (datetime.now - latest.scraped_at) > 300:
+    if (datetime.datetime.now() - latest.scraped_at).seconds > 300:
         return None
 
     return latest.data
