86df3a34ea51370195e9dd3141bc5b3c5079caca
Thomas Jongstra <jongstra@gmail.com>
2018-05-25 16:59:00 +0200

    removed run_index.sh and rewrote jenkins deploy import file

--- importer/deploy/import/import.sh
+++ importer/deploy/import/import.sh
@@ -15,21 +15,20 @@ trap 'dc down; dc kill ; dc rm -f -v' EXIT
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
+
+# Start database container.
 dc stop
 dc rm --force
 dc down
 # dc pull
 dc build
-
 dc up -d database
 
-# Download dump of quantillion database in objectstore @ directory "quantillion_dump".
-# dc run --rm importer python -m objectstore.databasedumps /data/ quantillion_dump --download-db
-
+# Wait until database container runs, and download data from objectstore.
 dc run --rm importer bash /app/deploy/docker-wait.sh
 dc run --rm importer python /app/main_ETL.py /data --download
 
-
+# Import Alpha database dump.
 #######################################################
 # THE ACTUAL HOTFIX. Should be commented out when Quantillion dump is correct.
 dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/google_raw_feb.dump
@@ -38,10 +37,24 @@ dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --i
 #dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/alpha_latest.dump
 #######################################################
 
-dc run --rm importer bash /app/run_import.sh
+# Tagged for removal
+#dc run --rm importer bash /app/run_import.sh
+
+# Create new tables in database.
+python scrape_api/models.py
+
+# Process data files, and write results to new tables in database.
+python main_ETL.py /data
+
+# Migrate the database.
 dc run --rm api python manage.py migrate
+
+# Run the analyzer.
 dc run --rm analyzer
+
+# Create a local database backup.
 dc exec -T database backup-db.sh citydynamics
+
 
 dc stop
 dc rm --force
