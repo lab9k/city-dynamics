ce7ae707f402f3483918364e8f5189fddcd77cfc
Timotheussie <schutte@silverdesign.nl>
2018-05-28 11:55:55 +0200

    Double gauge implemented

--- front/js/jquery-d3-area.js
+++ front/js/jquery-d3-area.js
@@ -48,8 +48,8 @@
 			// define line
 			graph.topline = d3.line()
 				.x(function(d) { return x(d.x); })
-				.y(function(d) { return y(d.y); })
-				.curve(d3.curveBasis);
+				.y(function(d) { return y(d.y); });
+				// .curve(d3.curveBasis);
 
 			// define area
 			graph.area = d3.area()
@@ -264,7 +264,7 @@
 					.attr("transform", 'translate(' + graph.currentPoint + ',0)')
 					// .attr('rx', 5)
 					// .attr('ry', 5)
-					.attr("fill", getColorBucket(graph.realtime))
+					.attr("fill", getColor(graph.realtime))
 					.attr("class", "realtime_bar_top");
 
 				// graph.realtime_bar_text = graph.svg.append("text")
