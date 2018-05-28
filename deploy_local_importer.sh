7d215690bc3b10ee0a52061a0cc260789efbd790
Thomas Jongstra <jongstra@gmail.com>
2018-05-28 16:46:06 +0200

    Deploy local script for importer.

--- deploy_local_importer.sh
+++ deploy_local_importer.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+# Wait until database container runs, and download data from objectstore.
+docker-compose run --rm importer bash /app/deploy/docker-wait.sh
+docker-compose run --rm importer python /app/main_ETL.py /data --download
+
+# Create new tables in database.
+python scrape_api/models.py
+
+# Process data files, and write results to new tables in database.
+python main_ETL.py /data
+
+# Migrate the database.
+docker-compose run --rm api python manage.py migrate
