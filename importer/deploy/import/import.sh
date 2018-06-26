07ac0fa9219bc5f9d81101afd6b0f4836e63d0a7
jeromecremers <jerome@xomnia.com>
2018-06-26 11:30:22 +0200

    restore importer dump

--- importer/deploy/import/import.sh
+++ importer/deploy/import/import.sh
@@ -31,25 +31,41 @@ dc up -d database
 
 # Wait until database container runs, and download data from objectstore.
 dc run --rm importer bash /app/deploy/docker-wait.sh
+
+### Comment out one of the sections between double hashtags. Top one: downloading and parsing data.
+### Bottom one: restore importer dump.
+
+# dc run --rm importer python /app/main.py /data --download
+
+# # Migrate/Create target tables the database.
+# dc run --rm api python manage.py migrate
+
+# # Import Alpha database dump.
+# #######################################################
+# # THE ACTUAL HOTFIX. Should be commented out when Quantillion dump is correct.
+# dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/quantillion_dump/validated_dump_feb.dump
+
+# # Restore alpha_latest instead of fallback (validated_dump_feb.dump) when the Quantillion scraper works correctly.
+# #dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/quantillion_dump/alpha_latest.dump
+# #######################################################
+
+# # Create new tables in database.
+# dc run --rm importer python scrape_api/models.py
+
+# # Process data files, and write results to new tables in database.
+# dc run --rm importer python main.py /data
+
+###
+###
+
+###
+###
 dc run --rm importer python /app/main.py /data --download
 
-# Migrate/Create target tables the database.
-dc run --rm api python manage.py migrate
+dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/dump_after_importer/20180626_dump_after_importer.dump
+###
+###
 
-# Import Alpha database dump.
-#######################################################
-# THE ACTUAL HOTFIX. Should be commented out when Quantillion dump is correct.
-dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/quantillion_dump/validated_dump_feb.dump
-
-# Restore alpha_latest instead of fallback (validated_dump_feb.dump) when the Quantillion scraper works correctly.
-#dc exec -T database pg_restore --username=citydynamics --dbname=citydynamics --if-exists --clean /data/quantillion_dump/alpha_latest.dump
-#######################################################
-
-# Create new tables in database.
-dc run --rm importer python scrape_api/models.py
-
-# Process data files, and write results to new tables in database.
-dc run --rm importer python main.py /data
 
 # Run the analyzer.
 dc run --rm analyzer
